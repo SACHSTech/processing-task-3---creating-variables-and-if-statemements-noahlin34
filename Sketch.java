beee8c83e75296d00937bc7c23e942e1dd3bb8d7
Noah <nglin34@gmail.com>
2022-03-04 12:18:36 -0500

    added flower, random movement, and time

--- Sketch.java
+++ Sketch.java
@@ -23,13 +23,68 @@ public class Sketch extends PApplet {
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    
+    float floatFlowerX;
+    float floatFlowerY;
+    float floatDistance;
+    float floatSize;
+    float floatColor1 = random(0, 255);
+    float floatColor2 = random(0, 255);
+    float floatColor3 = random(0, 255);
+    int intFillColor1 = 1;
+    int seconds = second();
+    int minutes = minute();
+    int hours = hour();
+    String hoursString;
+    String minutesString;
+    String secondsString;
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+  
+
+    floatFlowerX = random(0, width);
+    floatFlowerY = random(0, height);
+    floatSize = width/4;
+    floatDistance = floatSize/2;
+
+    //if the flower falls in the top right corner the background turns white
+    if (floatFlowerX < width/2 && floatFlowerY < height/2) {
+      background(255, 255, 255);
+    }
+    fill(floatColor1, floatColor2, floatColor3);
+    ellipse(floatFlowerX - floatDistance, floatFlowerY - floatDistance, floatSize, floatSize);
+
+    ellipse(floatFlowerX+floatDistance, floatFlowerY+floatDistance, floatSize, floatSize);
+
+    ellipse(floatFlowerX+floatDistance, floatFlowerY-floatDistance, floatSize, floatSize);
+
+    ellipse(floatFlowerX-floatDistance, floatFlowerY+floatDistance, floatSize, floatSize);
+    
+  
+
+    ellipse(floatFlowerX, floatFlowerY, floatSize, floatSize);
+
+    //square(floatFlowerX - floatFlowerX/6, floatFlowerY - floatFlowerY/6, floatSize/2);
+    square(floatFlowerX, floatFlowerY, floatSize/3);
+    square(floatFlowerX-floatDistance/2, floatFlowerY-floatDistance/2, floatSize/3);
+
+  
+
+    String time;
+
+    time = "the current time is: " + hours + ":" + minutes + ":" + seconds;
+
+    if (hours < 10) {
+      time = "the current time is: " + "0" + hours + ":" + minutes + ":" + seconds;
+    }
+    if (minutes < 10) {
+      time = "the current time is: " + hours + ":" + "0" + minutes + ":" + seconds;
+    }if (seconds < 10) {
+      time = "the current time is: " + hours + ":" + minutes + ":" + "0" + seconds;
+    }
+
+    text(time, 10, 120);
+
+    noLoop();
   }
   
   // define other methods down here.
